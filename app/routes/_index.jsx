719d3a1b026ac1e0b65d3f5e0c134809a9961d16
Trang Do <trangntdo@utexas.edu>
2023-08-02 19:39:33 -0500

    add featured collection on splash page try #2

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -60,32 +60,6 @@ export default function Index() {
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      {/* <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
-        {collections.nodes.map((collection) => {
-          return (
-            <Link to={`/collections/${collection.handle}`} key={collection.id}>
-              <div className="grid gap-4">
-                {collection?.image && (
-                  <Image
-                    alt={`Image of ${collection.title}`}
-                    data={collection.image}
-                    key={collection.id}
-                    sizes="(max-width: 32em) 100vw, 33vw"
-                    widths={[400, 500, 600, 700, 800, 900]}
-                    loaderOptions={{
-                      scale: 2,
-                      crop: 'center',
-                    }}
-                  />
-                )}
-                <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
-                  {collection.title}
-                </h2>
-              </div>
-            </Link>
-          );
-        })}
-      </div> */}
     </section>
   );
 }
