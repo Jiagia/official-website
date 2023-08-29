d0cd96be961988a3e6cfe29d2ae7ea6062500c14
Trang Do <trangntdo@utexas.edu>
2023-08-29 17:40:39 -0500

    Center the header on desktop view try 2

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -10,7 +10,7 @@ export function Header({cart, shop, menu, showSearch}) {
       className={`header flex items-center h-16 p-6 md:p-8 lg:p-12 sticky z-40 top-0 
           justify-center w-full leading-none gap-4 antialiased transition shadow-sm`}
     >
-      <nav className="flex items-center justify-evenly w-4/5 gap-12">
+      <nav className="flex items-center justify-between w-4/5 gap-12">
         <HeaderMenuMobileToggle />
         <HeaderMenuItem item={menu.items[0]} />
         <HeaderMenuItem item={menu.items[1]} />
