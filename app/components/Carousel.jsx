1fad92e1d45c80473f24e6228323f61d59a7371f
Trang Do <trangntdo@utexas.edu>
2023-11-12 21:01:42 -0600

    Add animation to the carousel in home page

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -5,15 +5,21 @@ export function Carousel({array = [], number = 3, wrap = true, id = "carousel", 
   const length = array.length - 1;
   const temp = array.concat(array);
   const [start, setStart] = useState(0);
+  const [state, setState] = useState("");
+  const [aniNo, setAniNo] = useState(true);
 
   let display = temp.slice(start, start + number);
 
   const prev = () => {
+    setState('-clicked-left-' + aniNo);
+    setAniNo(!aniNo);
     wrap
       ? setStart((start + length) % array.length)
       : setStart(start > 0 ? start - 1 : 0);
   };
   const next = () => {
+    setState('-clicked-right-' + aniNo);
+    setAniNo(!aniNo);
     wrap
       ? setStart((start + 1) % array.length)
       : setStart(start + number > length ? start : start + 1);
@@ -21,14 +27,15 @@ export function Carousel({array = [], number = 3, wrap = true, id = "carousel", 
 
   return (
     <>
-      <button className={"carousel-prev "+lbtnclass} onClick={prev}>{leftbtn}</button>
-      <div className={"carousel " + className} id={id}>
+      <button className={"carousel-prev z-10 "+lbtnclass} onClick={prev}>{leftbtn}</button>
+      <div className={`carousel carousel${state} ${className}`} id={id}>
         {display.map((element) => {
           // return <Image src={image} key={i} style={{float: 'left'}} width='25%' />;
+          
           return element;
         })}
       </div>
-      <button className={"carousel-next "+rbtnclass} onClick={next}>{rightbtn}</button>
+      <button className={"carousel-next z-10 "+rbtnclass} onClick={next}>{rightbtn}</button>
     </>
   );
 }
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -3,11 +3,18 @@ import {json} from '@shopify/remix-oxygen';
 import {Carousel} from '~/components/Carousel';
 import {Image} from '@shopify/hydrogen';
 import {FeaturedProductCard} from '../components/FeaturedCollection';
+// import {useState} from 'react';
+// import {motion, AnimatePresence} from 'framer-motion'
+import splashcss from '../styles/splash.css';
 
 // GLOBAL VARIABLES
 // Featured Collection
 const FeaturedCollectionHandle = 'season-4';
 const FeaturedCollectionNumber = 5;
+
+export const links = () => [
+  {rel: "stylesheet", href: splashcss},
+]
 
 export function meta({matches}) {
   // console.log(matches[0]?.data?.header?.shop);
@@ -56,6 +63,9 @@ export async function loader({context}) {
 export default function Index() {
   // hook that retrieves queries data from the loader function
   const {collection} = useLoaderData();
+
+  
+
   const images = [
     'https://picsum.photos/id/1/400/500',
     'https://picsum.photos/id/2/400/500',
@@ -64,9 +74,7 @@ export default function Index() {
     'https://picsum.photos/id/5/400/500',
   ];
 
-  const array = images.map((image, i) => {
-    return <Image src={image} key={i} style={{float: 'left'}} width="25%" />;
-  });
+  const array = images.map((image, i) => (<Image src={image} key={i} style={{float: 'left'}} width="25%" />));
 
   return (
     <div>
@@ -79,7 +87,8 @@ export default function Index() {
           lbtnclass="absolute h-full w-1/4"
           rightbtn={<div></div>}
           rbtnclass="absolute h-full w-1/4 right-0 top-0"
-          className="hidden md:block"
+          className="hidden md:block h-full"
+          id="gallery-desktop"
         />
         <span className="text-transparent">Jigia</span>
       </div>
@@ -91,7 +100,8 @@ export default function Index() {
           lbtnclass="absolute h-full w-1/4"
           rightbtn={<div></div>}
           rbtnclass="absolute h-full w-1/4 right-0 top-0"
-          className="md:hidden"
+          className="md:hidden h-full"
+          id="gallery-mobile"
         />
         <span> </span>
       </div>
@@ -102,29 +112,30 @@ export default function Index() {
         <div>2</div>
       </div>
       <div className="hidden md:flex px-4 md:px-6 lg:px-8">
-        <ProductCarousel collection={collection} number={4} />
+        <ProductCarousel collection={collection} number={4} id="prod-carousel-desktop" />
       </div>
       <div className="flex md:hidden px-4">
-        <ProductCarousel collection={collection} number={1} />
+        <ProductCarousel collection={collection} number={1} id="prod-carousel-mobile" />
       </div>
     </div>
   );
 }
 
-function ProductCarousel({collection, number}) {
+function ProductCarousel({collection, number, id = ''}) {
   return (
     <Carousel
       number={number}
       array={collection.products.nodes.map((product) => (
         <FeaturedProductCard key={product.id} product={product} />
       ))}
-      className={`grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-${number} relative`}
+      className={`grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-${number} relative w-full`}
       leftbtn={
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-8 h-8"><g data-name="91-Arrow Left"><path d="M16 32a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm0-30a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"/><path d="m18.29 24.71-8-8a1 1 0 0 1 0-1.41l8-8 1.41 1.41L12.41 16l7.29 7.29z"/></g></svg>
       }
       rightbtn={
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-8 h-8"><g data-name="92-Arrow Right"><path d="M16 32a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm0-30a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"/><path d="M13.71 24.71 12.3 23.3l7.29-7.3-7.3-7.29L13.7 7.3l8 8a1 1 0 0 1 0 1.41z"/></g></svg>
       }
+      id={id}
     />
   )
 }
