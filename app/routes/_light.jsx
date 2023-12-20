b32789592768a0d50de21693224553fa60051449
Trang Do <trangntdo@utexas.edu>
2023-12-20 13:38:27 -0600

    Add to + Update header/footer the world pages

---
+++ app/routes/_light.jsx
@@ -0,0 +1,163 @@
+import {defer, json} from '@shopify/remix-oxygen';
+import {useRef, useEffect, useState} from 'react';
+// import {useShopifyCookies} from '@shopify/hydrogen';
+import {
+  AnalyticsPageType,
+} from '@shopify/hydrogen';
+import {CartProvider, useCart} from '@shopify/hydrogen-react';
+// import {Seo} from `@shopify/hydrogen`;
+import {
+  Outlet,
+  useLoaderData,
+  useActionData,
+  useLocation,
+} from '@remix-run/react';
+import {Layout} from '../components/Layout';
+import {usePageAnalytics} from '~/hooks/usePageAnalytics';
+
+
+export async function loader({context}) {
+  const {storefront, session, cart} = context;
+  const customerAccessToken = await session.get('customerAccessToken');
+  const cookieConsent = await session.get('cookieConsent');
+  const publicStoreDomain = context.env.PUBLIC_STORE_DOMAIN;
+
+  // validate the customer access token is valid
+  const {isLoggedIn, headers} = await validateCustomerAccessToken(
+    customerAccessToken,
+    session,
+  );
+
+  // defer the cart query by not awaiting it
+  const cartPromise = cart.get();
+
+  // defer the footer query (below the fold)
+  const footerPromise = storefront.query(FOOTER_QUERY, {
+    cache: storefront.CacheLong(),
+    variables: {
+      footerMenuHandle: 'footer', // Adjust to your footer menu handle
+    },
+  });
+
+  // await the header query (above the fold)
+  const headerPromise = storefront.query(HEADER_QUERY, {
+    cache: storefront.CacheLong(),
+    variables: {
+      headerMenuHandle: 'main-menu', // Adjust to your header menu handle
+    },
+  });
+
+  return defer(
+    {
+      cart: cartPromise,
+      footer: await footerPromise,
+      header: await headerPromise,
+      isLoggedIn,
+      publicStoreDomain,
+      cookieConsent,
+      analytics: {
+        pageType: AnalyticsPageType.home,
+      },
+    },
+    {headers},
+  );
+}
+
+export default function LightLayout() {
+  const data = useLoaderData();
+
+  return (
+    <Layout {...data}>
+      <Outlet />
+    </Layout>
+  )
+}
+
+async function validateCustomerAccessToken(customerAccessToken, session) {
+  let isLoggedIn = false;
+  const headers = new Headers();
+  if (!customerAccessToken?.accessToken || !customerAccessToken?.expiresAt) {
+    return {isLoggedIn, headers};
+  }
+  const expiresAt = new Date(customerAccessToken.expiresAt);
+  const dateNow = new Date();
+  const customerAccessTokenExpired = expiresAt < dateNow;
+  if (customerAccessTokenExpired) {
+    session.unset('customerAccessToken');
+    headers.append('Set-Cookie', await session.commit());
+  } else {
+    isLoggedIn = true;
+  }
+
+  return {isLoggedIn, headers};
+}
+
+const MENU_FRAGMENT = `#graphql
+  fragment MenuItem on MenuItem {
+    id
+    resourceId
+    tags
+    title
+    type
+    url
+  }
+  fragment ChildMenuItem on MenuItem {
+    ...MenuItem
+  }
+  fragment ParentMenuItem on MenuItem {
+    ...MenuItem
+    items {
+      ...ChildMenuItem
+    }
+  }
+  fragment Menu on Menu {
+    id
+    items {
+      ...ParentMenuItem
+    }
+  }
+`;
+
+const HEADER_QUERY = `#graphql
+  fragment Shop on Shop {
+    id
+    name
+    description
+    primaryDomain {
+      url
+    }
+    brand {
+      logo {
+        image {
+          url
+        }
+      }
+    }
+  }
+  query Header(
+    $country: CountryCode
+    $headerMenuHandle: String!
+    $language: LanguageCode
+  ) @inContext(language: $language, country: $country) {
+    shop {
+      ...Shop
+    }
+    menu(handle: $headerMenuHandle) {
+      ...Menu
+    }
+  }
+  ${MENU_FRAGMENT}
+`;
+
+const FOOTER_QUERY = `#graphql
+  query Footer(
+    $country: CountryCode
+    $footerMenuHandle: String!
+    $language: LanguageCode
+  ) @inContext(language: $language, country: $country) {
+    menu(handle: $footerMenuHandle) {
+      ...Menu
+    }
+  }
+  ${MENU_FRAGMENT}
+`;
--- app/utils/components/Header.jsx
+++ app/utils/components/Header.jsx
@@ -8,9 +8,9 @@ export function Header({cart, shop, menu}) {
     <header
       role="banner"
       className={`header flex items-center h-16 p-6 md:p-8 lg:p-12 sticky z-40 top-0 
-          justify-center w-full leading-none gap-4 antialiased transition`}
+          justify-center w-full leading-none gap-4 antialiased transition shadow-sm`}
     >
-      <nav className="flex align-middle items-center justify-start content-start w-11/12 ">
+      <nav className="flex align-top items-start justify-between content-start w-11/12 ">
         <HeaderMenuMobileToggle />
         <NavLink
           to="/"
@@ -19,18 +19,17 @@ export function Header({cart, shop, menu}) {
           prefetch="intent"
           style={activeLinkStyle}
         >
-          {"Jiagia Studios"} 
+          {shop.name}
         </NavLink>
         <HeaderMenuItem item={menu.items[0]} />
         <HeaderMenuItem item={menu.items[1]} />
-        
         {menu.items[2] ? <HeaderMenuItem item={menu.items[2]} /> : null}
         {menu.items[3] ? <HeaderMenuItem item={menu.items[3]} /> : null}
 
-        {/* <nav className="header-ctas" role="navigation">
+        <nav className="header-ctas" role="navigation">
           <SearchToggle />
           <CartToggle cart={cart} />
-        </nav> */}
+        </nav>
       </nav>
     </header>
   );
