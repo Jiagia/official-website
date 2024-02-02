14eac34139b4a919289134740dfca39862c8fe39
Trang Do <trangntdo@utexas.edu>
2024-02-02 17:29:33 -0600

    Add show/hide feature for the text box on world page

--- app/routes/_light.world.the-laboratory.jsx
+++ app/routes/_light.world.the-laboratory.jsx
@@ -1,6 +1,7 @@
 import {useLoaderData} from '@remix-run/react';
 import {Image} from '@shopify/hydrogen-react';
 import {json} from '@shopify/remix-oxygen';
+import { useState } from 'react';
 import img1 from '../../public/Lab2.png'
 import img2 from '../../public/Lab3.png'
 import img3 from '../../public/Lab5.png'
@@ -32,6 +33,13 @@ export default function Laboratory() {
   const {season} = useLoaderData();
   console.log(season.metaobject);
   var page = season.metaobject;
+
+  // const [dropshow, setDropshow] = useState("hidden");
+  // const showBodyText = () => {
+  //   dropshow == "hidden" ? setDropshow("") : setDropshow("hidden")
+  // } 
+
+
   return (
     <div className="container mx-auto mb-16 p-8 md:p-10 xl:p-32">
       <div className="flex flex-col space-between items-center mx-10 mb-10 gap-5 text-center">
@@ -39,64 +47,46 @@ export default function Laboratory() {
         <p>{page.description.value}</p>
       </div>
       <div width="100%" className="flex flex-col text-xxs md:text-xs" style={{rowGap: "25px"}}>
-        {/* <div className="flex w-full text-xxs md:text-xs">
-          <div className="w-full flex flex-initial flex-col"> */}
         {page.rows.references.nodes.map((boxes, i) => (
           <div className="flex w-full flex-col md:flex-row" style={{rowGap: "25px", columnGap: "10px"}} key={i}>
             {boxes.boxes.references.nodes.map((box, j) => (
-              <div className='border border-black'  key={j}>
-                {/* <Image data={box.image.reference.image} /> */}
-                <img className='border border-black' src={box.image.reference.image.url} />
-                <div className=' p-4'>
-                  <div><b>{box.title.value}</b></div>
-                  <div><i>{box.subtitle.value}</i></div>
-                  <div className="mt-4">
-                    {box.description.value
-                      .split('\n')
-                      .map((desc, index) => (
-                      <div key={index}>{desc}</div>
-                    ))}
-                  </div>
-                </div>
-              </div>
+              <TextBox box={box} key={j} />
             ))}
           </div>
         ))}
-          {/* </div>
-        </div> */}
-      {/* </div>
-      <div className='flex flex-col'>
-        <div className='flex'>
-          <div>
-            <img src={img1} />
-            <div>
-              Hello <br />
-            </div>
-          </div>
-          <div>
-            <img src={img2} />
-            <div>
-              Hello <br />
-            </div>
-          </div>
-        </div>
-        <div className='flex'>
-          <div>
-            <img src={img3} />
-            <div>
-              Hello <br />
-            </div>
-          </div>
-          <div>
-            <img src={img4} />
-            <div>
-              Hello <br />
-            </div>
-          </div>
-      </div> */}
       </div> 
     </div>
   );
+}
+
+function TextBox({box}) {
+
+  const [dropshow, setDropshow] = useState("hidden");
+  const showBodyText = () => {
+    dropshow == "hidden" ? setDropshow("") : setDropshow("hidden")
+  } 
+
+  return (
+    <div className='border border-black'>
+      <img className='border border-black' src={box.image.reference.image.url} />
+      <div className=' p-4'>
+        <div>
+          <b>{box.title.value}</b>
+          <button onClick={showBodyText}>+</button>
+        </div>
+        <div>
+          <i>{box.subtitle.value}</i> 
+        </div>
+        <div className={`mt-4 ${dropshow}`}>
+          {box.description.value
+            .split('\n')
+            .map((desc, index) => (
+            <div key={index}>{desc}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const METAOBJECT_QUERY = `#graphql
